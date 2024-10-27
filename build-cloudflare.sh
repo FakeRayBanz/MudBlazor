1dd0332c377036f258199037f6086dbc71d10739
Matthew Parker [SSW] <61717342+MattParkerDev@users.noreply.github.com>
2024-10-27 15:13:28 +1000

    Update build-cloudflare.sh

--- build-cloudflare.sh
+++ build-cloudflare.sh
@@ -5,4 +5,5 @@ chmod +x dotnet-install.sh
 ./dotnet/dotnet --version
 ./dotnet/dotnet workload install wasm-tools
 ./dotnet/dotnet workload list
+./dotnet/dotnet run -c Release ./src/MudBlazor.Docs.Compiler/MudBlazor.Docs.Compiler.csproj
 ./dotnet/dotnet publish ./src/MudBlazor.Docs.Wasm/MudBlazor.Docs.Wasm.csproj -c Release -o output
